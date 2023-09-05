c70c2f145cf3a3bee50b7d5e584411c5996b1cf2
Kroos31 <130866838+Kroos31@users.noreply.github.com>
2023-09-05 23:50:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "Hey JUDE!"
+    return "Hey JUDE Bellingham! The new KING"
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
